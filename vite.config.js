64d8e8d6c6d65569f0b8fe39037949c2c73673a6
Imene lc <imenelc18@gmail.com>
2026-05-24 23:44:03 +0100

    projet pret pour deployement

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Augmente la limite pour ignorer le warning inutile
+    rollupOptions: {
+      output: {
+        // Sépare les grosses librairies (comme Lucide, React) de ton code pour que ça charge plus vite
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
